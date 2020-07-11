e3f862d103f1b3f02208846ac33c81c9502be8e2
Ax1-Team <ax1ahmedelsheekh09@gmail.com>
2020-07-12 00:14:58 +0200

    Ax1-Team

---
+++ Ax1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import fileinput
+
+Ahmed = raw_input ("File?")
+Ax1 = raw_input ("Out")
+
+os.system ("bash-obfuscate " + Ahmed + " -o " + Ax1)
+ptint ("COMPLETE ")
